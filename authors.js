3d7c4dc79f70f2f22a0ac2615b0d2c96a6a4cf73
Edouard-Germain <edangermain@gmail.com>
2021-12-06 16:04:57 +0100

    exercice fin

---
+++ authors.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const port = 5000
+const app = express();
+
+const authors = [
+    {
+        name: "Lawrence Nowell",
+        nationality: "UK",
+        books: ["Beowulf"]
+    },
+    {
+        name: "William Shakespeare",
+        nationality: "UK",
+        books: ["Hamlet", "Othello", "Romeo and Juliet", "MacBeth"]
+    },
+    {
+        name: "Charles Dickens",
+        nationality: "US",
+        books: ["Oliver Twist", "A Christmas Carol"]
+    },
+    {
+        name: "Oscar Wilde",
+        nationality: "UK",
+        books: ["The Picture of Dorian Gray", "The Importance of Being Earnest"]
+    },
+]
+
+// app.get('/',(req,res) =>{
+//     res.send("hello")
+// })
+
+app.get('/authors/:id',(req,res) => {
+    const { id } = req.params
+    const num = parseInt(id) - 1
+    const name = authors[num].name
+    const country = authors[num].nationality
+    res.send(`${country},${name}`)
+    console.log(country,name)
+
+})
+
+app.get('/authors/:id/books', (req,res)=>{
+    const { id } = req.params
+    const num = parseInt(id) - 1
+    const name = authors[num].name
+    const books = authors[num].books.join()
+    res.send(`${name}, ${books}`)
+})
+
+app.get ('/json/authors/:id',(req,res) =>{
+    const {id} = req.params
+    const num = parseInt(id)-1
+    const name = authors[num].name
+    const nationality = authors[num].nationality
+    const obj = {nationality, name}
+    const array = JSON.stringify(obj)
+    res.send(`${array}`)
+
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+  })
